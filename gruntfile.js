e4019ed5ea6e3432e75a1223f70c73c7404da932
Chonlasith Jucksriporn <chonlasith@gmail.com>
2019-07-16 23:33:58 +0700

    Add several slides

--- gruntfile.js
+++ gruntfile.js
@@ -150,6 +150,10 @@ module.exports = grunt => {
             // - https://github.com/hakimel/reveal.js#configuration
             // - https://github.com/hakimel/reveal.js#dependencies
             Reveal.initialize({
+                fragments: true,
+                fragmenntInURL: true,
+                hash: true,
+                history: true,
                 dependencies: [
                     { src: 'plugin/markdown/marked.js' },
                     { src: 'plugin/markdown/markdown.js' },
